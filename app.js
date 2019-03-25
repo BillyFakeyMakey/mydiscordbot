4a6f5281548dd4ee8d0a9ced7a8ce7113582927a
BillyFakeyMakey <43906321+BillyFakeyMakey@users.noreply.github.com>
2019-03-25 01:14:23 +0100

    Dodano kick
    dodano kick... powinno dzialac

--- app.js
+++ app.js
@@ -33,6 +33,34 @@ client.on('message', message => {
     message.reply(error.data)
   });
 }
+    if (msg.startsWith(prefix + 'KICK')) {
+        let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    if(!kUser) return message.channel.send("Nie mogę znaleźć tego użytkownika!");
+    let kReason = args.join(" ").slice(22);
+    if(!kReason) return message.channel.send("Podaj powód");
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("No can do pal!");
+    if(kUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("Ta osoba nie moze zostac wyrzucona!");
+
+    let kickEmbed = new Discord.RichEmbed()
+    .setDescription("~Kick~")
+    .setColor("#e56b00")
+    .addField("Wyrzucony użytkownik", `${kUser} z ID ${kUser.id}`)
+    .addField("Wyrzucony przez", `<@${message.author.id}> z ID ${message.author.id}`)
+    .addField("Wyrzucony w", message.channel)
+    .addField("Kiedy", message.createdAt)
+    .addField("Powód", kReason);
+
+    let kickChannel = message.guild.channels.find(`name`, "bansandkicks");
+    if(!kickChannel) return message.channel.send("Nie mogę znalezc kanalu banów i kicków.");
+
+    message.guild.member(kUser).kick(kReason);
+    kickChannel.send(kickEmbed);
+
+    return;
+        
+        
+        
+    }
   
   
     if (msg.startsWith(prefix + 'POGODA')) { 
